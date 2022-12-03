8841832eb30ec9d8c4ca0a698e2e432fac2ae865
AlexandrMoiseenko <moiseenko.112@mail.ru>
2022-12-03 19:29:10 +0300

    программа по определению кубов числа N

---
+++ Ex_003/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход (N) число и
+// выдает таблицу кубов чисел от 1 до N. Например:
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Hello(string message)                           // Метод вывода приглашения и ввода числа
+{
+    Console.Write(message + "> ");                  // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine());     // Ввод числа
+}
+
+int number = Hello("Введите число ");
+
+int count = 0;
+
+while(count <= number)
+{
+    int cubecount = count * count * count;
+    Console.Write(cubecount + " ");
+    count++;
+}
